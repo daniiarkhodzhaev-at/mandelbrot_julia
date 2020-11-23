2283785620416dfc3a53a81327804ec85d549628
Alexander Danyarkhodzhaev <alexdany657@gmail.com>
2020-11-23 15:28:30 +0300

    Fixed some bugs

--- calc.py
+++ calc.py
@@ -2,17 +2,17 @@
 
 NUM_STEPS = 1024;
 
-WIDTH = 800
-HEIGHT = 800
-
-# tmp solution
-COLORS = ((0.61, 0.15, 0.69),
-          (0.25, 0.32, 0.71),
-          (0.00, 0.66, 0.96))
+WIDTH = 400
+HEIGHT = 400
 
 def __scale(color: tuple, a: float) -> tuple:
     assert len(color) == 3
     return (color[0] * a, color[1] * a, color[2] * a)
+
+# tmp solution
+COLORS = (__scale((0.61, 0.15, 0.69), 255),
+          __scale((0.25, 0.32, 0.71), 255),
+          __scale((0.00, 0.66, 0.96), 255))
 
 def __color_sum(color1: tuple, color2: tuple) -> tuple:
     assert len(color1) == 3
@@ -24,20 +24,20 @@ def _get_color(n: int, colors: tuple) -> tuple:
         return (0.0 ,0.0 ,0.0)
     
     i = n % 20 / 20
-    cel = n / 20 % 3
+    cel = n // 20 % 3
 
     color1 = colors[0]
     color2 = colors[1]
     color3 = colors[2]
 
     if (cel == 0):
-        return __color_sum(__scale(color1, i) + __scale(color2, (1 - i)))
+        return __color_sum(__scale(color1, i), __scale(color2, (1 - i)))
 
     if (cel == 1):
-        return __color_sum(__scale(color3, i) + __scale(color1, (1 - i)))
+        return __color_sum(__scale(color3, i), __scale(color1, (1 - i)))
 
     if (cel == 2):
-        return __color_sum(__scale(color2, i) + __scale(color3, (1 - i)))
+        return __color_sum(__scale(color2, i), __scale(color3, (1 - i)))
 
     return (0.0, 0.0, 0.0)
 
@@ -51,9 +51,9 @@ def _julia_number_of_term(x: float, y: float, cx: float, cy: float) -> int:
     return i
 
 def _mandelbrot_number_of_term(x: float, y: float) -> int:
-    return julia_number_of_term(0, 0, x, y)
+    return _julia_number_of_term(0, 0, x, y)
 
-def calc(mx: float, my: float, dx: float, dy: float,
+def calc(mx: float, my: float, dmx: float, dmy: float,
         cx: float, cy: float,
         jx: float, jy: float, djx: float, djy: float) -> tuple:
     """
@@ -79,12 +79,12 @@ def calc(mx: float, my: float, dx: float, dy: float,
     for i in range(WIDTH):
         for j in range(HEIGHT):
             m_points[i][j] = _get_color(_mandelbrot_number_of_term(
-                mx - dmx / 2 + dmx * i / HEIGHT, my - dmy / 2 + dmy * j / HEIGHT))
+                mx - dmx / 2 + dmx * i / HEIGHT, my - dmy / 2 + dmy * j / HEIGHT), COLORS)
     j_points = [[(0, 0, 0) for _ in range(HEIGHT)] for _ in range(WIDTH)]
     for i in range(WIDTH):
         for j in range(HEIGHT):
-            m_points[i][j] = _get_color(_mandelbrot_number_of_term(
-                jx - djx / 2 + djx * i / HEIGHT, jy - djy / 2 + djy * j / HEIGHT, cx, cy))
+            j_points[i][j] = _get_color(_julia_number_of_term(
+                jx - djx / 2 + djx * i / HEIGHT, jy - djy / 2 + djy * j / HEIGHT, cx, cy), COLORS)
     return (m_points, j_points)
 
 if (__name__ == "__main__"):
